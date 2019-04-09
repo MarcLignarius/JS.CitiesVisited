de2daddeee1d89a1da77cea4b3861f01b2ef4a25
Marc Davies <marcdaviesriot@gmail.com>
2019-04-08 22:16:06 -0700

    track changes in gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ var portland = new places("United States", "Multnomah Falls", "See a Timbers gam
 var kyoto = new places("Japan", "Monkey Mountain", "Drink shochu")
 var bordeaux = new places("France", "Rue Sainte Catherine", "Drink wine")
 
+// business logic for Places
+var portland = new places("United States", "Multnomah Falls", "See a Timbers game")
+var kyoto = new places("Japan", "Monkey Mountain", "Drink shochu")
+var bordeaux = new places("France", "Rue Sainte Catherine", "Drink wine")
+
 function places(country, landmark, activity) {
   this.country = country,
   this.landmark = landmark,
@@ -12,11 +17,6 @@ function places(country, landmark, activity) {
 places.prototype.totalInfo = function() {
 return "<li>" + "Country: " + this.country + "</li>" + "<li>" + "Landmark: " + this.landmark + "</li>" + "<li>" + "Activity: " + this.activity + "</li>";
 }
-
-// Place.forEach(function(place) {
-//   console.log(this.country + this.landmark);
-//   console.log("\n");
-// });
 
 // user interface logic
 
